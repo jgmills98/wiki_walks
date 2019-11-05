6c3260079dd1d28cf4717a96518c02ef1349ed8a
J M <jgmills1998@gmail.com>
2019-11-04 22:21:31 -0500

    Changed db functions to work with both collections

--- util.py
+++ util.py
@@ -1,27 +1,45 @@
 import json
 from pymongo import MongoClient
 
-
 def open_json(file):
     with open("{}.json".format(file)) as json_file:
         json_data = json.load(json_file)
         return json_data
 
-def connect_db(config):
+def connect_db(config, collection):
     client = MongoClient(config["db_url"])
-    return client["wikidb"].items
+
+    if collection == "items":
+        return client["wikidb"].items
+    elif collection == "results":
+        return client["wikidb"].results
 
 config = open_json("config")
-db = connect_db(config)
+db_items = connect_db(config, "items")
+db_results = connect_db(config, "results")
+
 
 #searches database for name that matches the title
-def check_db(title):
-    query_res = db.find_one({"name":title})
-    if query_res:
-        return query_res
-    else:
-        return []
+def check_db(title, collection):
 
-def insert_db(title, data):
-    elem = {"name": title, "links": data}
-    db.insert_one(elem)
+    if collection == "items":
+        query_res = db_items.find_one({"name":title})
+        if query_res:
+            return query_res
+        else:
+            return []
+
+    elif collection == "results":
+        query_res = db_results.find_one({"name":title})
+        if query_res:
+            return query_res
+        else:
+            return []
+
+def insert_db(title, data, collection):
+    if collection == "items":
+        elem = {"name": title, "links": data}
+        db_items.insert_one(elem)
+    elif collection == "results":
+        elem = {"name": title, "data": data}
+        db_results.insert_one(elem)
