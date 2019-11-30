5e04cb793cbb52fc736aa4e14a0c70e40fb0cc4f
J M <jgmills1998@gmail.com>
2019-11-29 23:45:32 -0500

    fixed % bug, added boolean matrix code, more db functions

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import json
 import sys
 import queue
 import argparse
+import re
+import random
+import numpy as np
 
 from urllib.parse import unquote
 from pymongo import MongoClient
@@ -14,8 +17,11 @@ from util import *
 #href="/wiki/.+?(?=\")"
 
 wiki_url = "https://en.wikipedia.org/wiki/{}"
+random_url = "https://en.wikipedia.org/wiki/Special:Random"
 
 "href=/wiki/"
+
+perc_placeholder = "**___**"
 
 filter_list = [
     "#",
@@ -39,22 +45,36 @@ filter_list = [
 ]
 
 #curl requests to the wiki title and returns a set of all inter wiki links from that title
-def wiki_curl(title, get_title=False):
+def wiki_curl(title, get_title=False, random=False):
     formatted_title = title.replace(" ","_")
 
-    curl_url = wiki_url.format(formatted_title)
+    curl_url = wiki_url.format(formatted_title) if random == False else random_url
 
     req = requests.get(url=curl_url)
-
     req_result = bs4.BeautifulSoup(req.text,'html.parser')
-
+    
     # if "Wikipedia does not have an article with this exact name" in req_result.get_text():
     #     print("\"{}\" does not exist on Wikipedia. Please try fixing the capitalization or any other typo.".format(formatted_title))
     #     sys.exit()
 
+    if get_title == True:
+        found_title = req_result.find(id="firstHeading").text
+        if "% " in found_title:
+            perc_index = found_title.find("%")
+            found_title = found_title[:perc_index + 1] + "25" + found_title[perc_index + 1:]
+        
+        #wiki pages are underscore sperated for titles
+        found_title = ("_").join(found_title.split(" "))
+
+        return found_title
+
+
     return req_result
 
 def get_wiki_links(title):
+    if "%_" in title:
+        perc_index = title.find("%_")
+        title = title[:perc_index + 1] + "25" + title[perc_index + 1:] 
 
     wiki_request = wiki_curl(title)
 
@@ -63,11 +83,21 @@ def get_wiki_links(title):
     links = []
 
     for val in req_result.find_all('a', href=True):
-
+        
         href_str = val.get('href')
 
+
+        if "%25" in href_str:
+            href_str = href_str.replace("%25",perc_placeholder)
+        
         #remove url encoding from string
         href_str = unquote(href_str)
+
+        if perc_placeholder in href_str:
+            href_str = href_str.replace(perc_placeholder, "%25")
+
+        if re.search(r"\%\$(\w+)\%",href_str):
+            continue
 
         if any(x in href_str for x in filter_list) or not href_str.startswith("/wiki/"):
             continue
@@ -78,11 +108,14 @@ def get_wiki_links(title):
     return links
 
 def get_title(title):
-
+    print(title)
     wiki_request = wiki_curl(title)
 
     found_title = wiki_request.find(id="firstHeading").text
-
+    if "% " in found_title:
+        perc_index = found_title.find("%")
+        found_title = found_title[:perc_index + 1] + "25" + found_title[perc_index + 1:]
+    
     #wiki pages are underscore sperated for titles
     found_title = ("_").join(found_title.split(" "))
 
@@ -105,13 +138,22 @@ def check_search(start, end):
     name_str = start + " " + end
     check = check_db(name_str, "results")
     return check
-    
-def run_search(start, end):
-    
+
+def get_n_random(n):
+    rand_list = []
+    for i in range(0,n):
+        rand_list.append(wiki_curl("",True,True))
+    # print(wiki_curl("",True,True))
+    return rand_list
+
+def run_search(start, end, max_depth):
+    if start == end:
+        return [],0
+
     print("Finding \"{}\" from \"{}\"".format(end, start))
 
     sentinal_node = None
-    
+
     traveled = {}
 
     parent_dict = {}
@@ -125,7 +167,9 @@ def run_search(start, end):
     exit_loop = False
 
     while(not qu.empty()):
-        
+        if depth == max_depth+1:
+            return [], -1
+
         query_title = qu.get()
 
         if query_title == sentinal_node:
@@ -142,26 +186,50 @@ def run_search(start, end):
         #check if we have seen this title before
         if query_title in traveled:
             continue
-        
-        print("{} {} {}".format(query_title, qu.qsize(), depth))
+
+        print("{} {} {} {}".format(n, query_title, qu.qsize(), depth))
         n += 1
-        
+
         #add title to traveled dict
         traveled[query_title] = 1
+        try:
+            results = get_links(query_title)
+        except IndexError:
+            reverse_delete(query_title)
+            parent = parent_dict[query_title]
+            new_links = get_links(parent)
+            fix_arr = []
+            while not qu.empty():
+                fix_arr.append(qu.get())
+            for link in new_links:
+                qu.put(link)
+            for link in fix_arr:
+                qu.put(link)
+            continue
 
-        results = get_links(query_title)
-
+        deleted = False
         for res in results:
+            if "% " in res or "%_" in res:
+
+                perc_index = res.find("%")
+                res = res[:perc_index + 1] + "25" + res[perc_index + 1:]
+                if deleted == False:
+                    db_items.delete_one({"name":query_title})
+                    deleted = True
+                    print("deleting {} from db because of {}".format(query_title, res))
+                # sys.exit()
+
+
             if res == end:
                 parent_dict[end] = query_title
                 print("Found \"{}\" in {} clicks".format(end, depth))
                 exit_loop = True
-                break   
+                break
 
             if res not in parent_dict:
                 parent_dict[res] = query_title
-            qu.put(res)
-        
+                qu.put(res)
+
         if exit_loop:
             break
 
@@ -170,6 +238,41 @@ def run_search(start, end):
     store_result(start, end, depth, route)
 
     return route, depth
+
+def iterative_deep(start, end, depth):
+    return
+
+def matrix_calc(n, max_depth):
+    titles = get_n_random(n)
+    # print(titles)
+    matrix = np.zeros((n,n))
+    for i in range(0,n):
+        for j in range(0,n):
+            check = check_search(titles[i], titles[j])
+
+            if check == []:
+                route, depth = run_search(titles[i], titles[j], max_depth)
+            else:
+                route, depth = get_result_data(check)
+
+            # route,depth = run_search(titles[i],titles[j], max_depth)
+            if depth == -1:
+                matrix[i][j] = 0
+            else:
+                matrix[i][j] = 1
+    print(titles)
+    for row in matrix:
+        print(row)
+
+    results = []
+    for row in matrix:
+        count = 0
+        for item in row:
+            count += item
+        results.append(count/n)
+    print(results)
+
+    return
 
 def store_result(start, end, depth, route):
     data = {}
@@ -213,25 +316,27 @@ def get_args():
     return arg.parse_args()
 
 def main():
-    args = get_args()
-    print(args)
-    # start = sys.argv[1]
-    # end = sys.argv[2]
+    # args = get_args()
+    # print(args)
 
     #check wiki to make get the real title (e.g. if ww2 is typed in, the actual title is World_War_II)
-    actual_start = get_title(args.start)
-    actual_end = get_title(args.end)
+    # actual_start = get_title(args.start)
+    # actual_end = get_title(args.end)
 
-    check = check_search(actual_start, actual_end)
+    # check = check_search(actual_start, actual_end)
 
-    if check == [] or args.requery == True:
-        route, depth = run_search(actual_start, actual_end)
-    else:
-        print("Route already done, displaying previous results")
-        route, depth = get_result_data(check)
+    # if check == [] or args.requery == True:
+    #     route, depth = run_search(actual_start, actual_end)
+    # else:
+    #     print("Route already done, displaying previous results")
+    #     route, depth = get_result_data(check)
 
-    print_info(actual_start, actual_end, depth, route)
+    # print_info(actual_start, actual_end, depth, route)
 
+    # iterative_deep(actual_start, actual_end, 1)
+
+    matrix_calc(10,2)
+    # print(get_n_random(10))
 
 if __name__ == '__main__':
     main()
--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 import json
+import  time
+import random
 from pymongo import MongoClient
+import pymongo
 
 def open_json(file):
     with open("{}.json".format(file)) as json_file:
@@ -21,20 +24,24 @@ db_results = connect_db(config, "results")
 
 #searches database for name that matches the title
 def check_db(title, collection):
+    try:
+        if collection == "items":
+            query_res = db_items.find_one({"name":title})
+            if query_res:
+                return query_res
+            else:
+                return []
 
-    if collection == "items":
-        query_res = db_items.find_one({"name":title})
-        if query_res:
-            return query_res
-        else:
-            return []
-
-    elif collection == "results":
-        query_res = db_results.find_one({"name":title})
-        if query_res:
-            return query_res
-        else:
-            return []
+        elif collection == "results":
+            query_res = db_results.find_one({"name":title})
+            if query_res:
+                return query_res
+            else:
+                return []
+    except pymongo.errors.AutoReconnect:
+        "DB disconnect. Waiting 15 seconds before reconnecting"
+        time.sleep(15)
+        return check_db(title, collection)
 
 def insert_db(title, data, collection):
     if collection == "items":
@@ -43,3 +50,32 @@ def insert_db(title, data, collection):
     elif collection == "results":
         elem = {"name": title, "data": data}
         db_results.insert_one(elem)
+
+#searched for items with the title in its list and deletes it from the db
+def reverse_delete(title):
+    query = {"links":{"$all":[title]}}
+
+    search = db_items.find(query)
+    for item in search:
+        name = item["name"]
+        print("Deleting: {} from db".format(name))
+        db_items.delete_one({"name":name})
+    # db_items.delete_many(query)
+    query = {"name":title}
+    print("Deleting: {} from db".format(title))
+    db_items.delete_one(query)
+
+def get_db_size():
+    return db_items.count()
+
+def get_n_titles(n):
+
+    # db_size = get_db_size()
+    # rand_int = random.randint(0,db_size-1)
+    rand_titles = db_items.aggregate([{"$sample":{"size":n}}])
+    title_list = []
+    for title in rand_titles:
+        title_list.append(title["name"])
+
+    return title_list
+    
